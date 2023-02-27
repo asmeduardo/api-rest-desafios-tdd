a503350f772fe1ac4855e0399ae197752200e03c
Eduardo Melo <eduardo.melo01992@gmail.com>
2023-02-27 14:49:39 -0300

    Tests passing

--- bds01/src/main/java/com/devsuperior/bds01/entities/Employee.java
+++ bds01/src/main/java/com/devsuperior/bds01/entities/Employee.java
@@ -7,10 +7,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_employee")
-public class Employee {
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
