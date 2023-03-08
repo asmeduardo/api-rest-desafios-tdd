ca29f2cab9178b73e55c22b91df9162f45793bb6
Eduardo Melo <eduardo.melo01992@gmail.com>
2023-03-08 13:01:24 -0300

    Tests passing

---
+++ bds04/src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> page = eventRepository.findAll(pageable);
+        return page.map(EventDTO::new);
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event event = new Event();
+        event.setName(dto.getName());
+        event.setDate(dto.getDate());
+        event.setUrl(dto.getUrl());
+        event.setCity(new City(dto.getCityId(), null));
+        event = eventRepository.save(event);
+        return new EventDTO(event);
+    }
+}
